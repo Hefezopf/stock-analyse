3836a90dc588234b521079ee9e563b4f1ebf0ba7
Hefezopf <markus@hopf-it.de>
2020-11-28 14:38:17 +0100

    uuu

--- set-variables.sh
+++ set-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ratedEnv=overrated
-#unset ratedEnv
-ratedVar="${ratedEnv-underrated}"
-echo $ratedVar
-./analyse.sh 'DB1.XETRA BMW.XETRA' 2 offline $ratedVar
+percentageEnv=2
+#percentageInput=3
+#unset percentageEnv
+#unset percentageInput
+percentageVar=${percentageInput:-$percentageEnv}
+#percentageVar="${percentageEnv-percentageInput}"
+#percentageVar="${percentageEnv-3}"
+echo percentage $percentageVar
+./analyse.sh 'DB1.XETRA BMW.XETRA' $percentageVar offline underrated
