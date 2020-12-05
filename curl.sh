807e08d8b0bd9d76b08bced84d2fab0078d4fd66
Hefezopf <markus@hopf-it.de>
2020-12-05 04:32:25 +0100

    dev

---
+++ curl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#./analyse.sh $1 $2 $3 $4 $5
+set +x
+curl -H "Authorization: token $GITHUB_TOKEN" -H / 
+'Accept: application/vnd.github.everest-preview+json' / 
+"https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" / 
+-d '{"event_type": "curl", "client_payload": {"symbols": "MSF.XETRA", "percentage": "1", "query": "online", "rated": "underrated", "stochastic": "40"}'
