defb15fdf10112987fb14bb393cc00a849497346
Hefezopf <markus@hopf-it.de>
2020-12-05 06:33:59 +0100

    14

--- analyse.sh
+++ analyse.sh
@@ -148,18 +148,18 @@ stochasticOfDays() {
 	do
 		headLines=$(echo $((100-$i)))
 		head -n$headLines data/values.${symbol}.txt | tail -"${1}" > $stochasticFile
-		lastStochastic14Raw=$(head -n 1 $stochasticFile)
-		lowestStochastic14Raw=$(sort $stochasticFile | head -n 1)
-		highestStochastic14Raw=$(sort -r $stochasticFile | head -n 1)
-		greaterThen 1 $highestStochastic14Raw $lowestStochastic14Raw; validStochastik=$?
+		lastStochasticRaw=$(head -n 1 $stochasticFile)
+		lowestStochasticRaw=$(sort $stochasticFile | head -n 1)
+		highestStochasticRaw=$(sort -r $stochasticFile | head -n 1)
+		greaterThen 1 $highestStochasticRaw $lowestStochasticRaw; validStochastik=$?
 		if [ "$validStochastik" = 1 ]; then
 			# Formula=((C – Ln )/( Hn – Ln )) * 100
-			lastStochasticPrice=$(echo "$lastStochastic14Raw $lowestStochastic14Raw $highestStochastic14Raw" | awk '{print ( ($1 - $2) / ($3 - $2) ) * 100}')
+			lastStochasticPrice=$(echo "$lastStochasticRaw $lowestStochasticRaw $highestStochasticRaw" | awk '{print ( ($1 - $2) / ($3 - $2) ) * 100}')
 		else
 			lastStochasticPrice=100
 		fi
-
-		stochasticList=$(echo $stochasticList $lastStochasticPrice",")
+	    round ${lastStochasticPrice} 0; lastStochasticPriceRounded=$?
+		stochasticList=$(echo $stochasticList $lastStochasticPriceRounded",")
 		i=$(( i + 1 ))
 	done
 }
@@ -221,8 +221,7 @@ do
 	stochasticOfDays $stochasticInDays14
 	stochasticList14=$stochasticList
 
-	# Some other Stochastics
-	round ${lastStochasticPrice} 0; stochasticRounded14=$?
+	# Stochastics percentage
 	stochasticPercentageLower=$stochasticPercentageParam
 	stochasticPercentageUpper=$(echo "$stochasticPercentageLower" | awk '{print (100 - $1)}')
 
@@ -247,9 +246,9 @@ do
 		# Overrated
 		resultOverrated=""
 		if [ "$ratedParam" = 'overrated' ]; then
-			if [ "$stochasticRounded14" -gt "$stochasticPercentageUpper" ] && [ "$lastOverAgv18" = 1 ] && [ "$lastOverAgv38" = 1 ] && [ "$lastOverAgv100" = 1 ] && 
+			if [ "$lastStochasticPriceRounded" -gt "$stochasticPercentageUpper" ] && [ "$lastOverAgv18" = 1 ] && [ "$lastOverAgv38" = 1 ] && [ "$lastOverAgv100" = 1 ] && 
 			   [ "$agv18OverAgv38" = 1 ] && [ "$agv38OverAgv100" = 1 ] && [ "$agv18OverAgv100" = 1 ]; then
-				resultOverrated="- Overrated: $symbol last $last EUR is more then $percentageLesserFactor over average18: $average18 EUR and average38: $average38 EUR and over average100: $average100 EUR. Stochastic14 is $stochasticRounded14"
+				resultOverrated="- Overrated: $symbol last $last EUR is more then $percentageLesserFactor over average18: $average18 EUR and average38: $average38 EUR and over average100: $average100 EUR. Stochastic14 is $lastStochasticPriceRounded"
 				echo $resultOverrated
 				echo "<br>" >> $resultFile
 				echo "\"http://www.google.com/search?tbm=fin&q=${symbol}\" " >> $resultFile
@@ -260,9 +259,9 @@ do
 		# Underrated
 		resultUnderrated=""
 		if [ "$ratedParam" = 'underrated' ]; then
-			if [ "$stochasticRounded14" -lt "$stochasticPercentageLower" ] && [ "$lastUnderAgv18" = 1 ] && [ "$lastUnderAgv38" = 1 ] && [ "$lastUnderAgv100" = 1 ] && 
+			if [ "$lastStochasticPriceRounded" -lt "$stochasticPercentageLower" ] && [ "$lastUnderAgv18" = 1 ] && [ "$lastUnderAgv38" = 1 ] && [ "$lastUnderAgv100" = 1 ] && 
 			   [ "$agv18UnderAgv38" = 1 ] && [ "$agv38UnderAgv100" = 1 ] && [ "$agv18UnderAgv100" = 1 ]; then
-				resultUnderrated="+ Underrated: $symbol last $last EUR is more then $percentageGreaterFactor under average18: $average18 EUR and under average38: $average38 EUR and under average100: $average100 EUR. Stochastic14 is $stochasticRounded14"
+				resultUnderrated="+ Underrated: $symbol last $last EUR is more then $percentageGreaterFactor under average18: $average18 EUR and under average38: $average38 EUR and under average100: $average100 EUR. Stochastic14 is $lastStochasticPriceRounded"
 				echo $resultUnderrated
 				echo "<br>" >> $resultFile
 				echo "\"http://www.google.com/search?tbm=fin&q=${symbol}\" " >> $resultFile
@@ -311,7 +310,7 @@ do
 	echo "&nbsp;Average 18:<b>" $average18 "&#8364;</b>" >> $indexSymbolFile
 	echo "&nbsp;Average 38:<b>" $average38 "&#8364;</b>" >> $indexSymbolFile
 	echo "&nbsp;Average 100:<b>" $average100 "&#8364;</b>" >> $indexSymbolFile
-	echo "&nbsp;Stochastic 14:<b>" $stochasticRounded14 "</b></p>" >> $indexSymbolFile
+	echo "&nbsp;Stochastic 14:<b>" $lastStochasticPriceRounded "</b></p>" >> $indexSymbolFile
 	echo "<p>Result:</p>" >> $indexSymbolFile
 	echo "<p><b>" $resultUnderrated "</b></p>" >> $indexSymbolFile
 	cat js/indexPart11.html >> $indexSymbolFile
