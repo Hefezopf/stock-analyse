c643e2ea36e8d10bbd6d168450d4223cac12cd97
Hefezopf <markus@hopf-it.de>
2020-12-05 05:13:18 +0100

    wf

--- curl.sh
+++ curl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#./analyse.sh $1 $2 $3 $4 $5
+#$1 $2 $3 $4 $5
+#./curl.sh "INL.XETRA" 1 offline underrated 20
 set -x
-curl -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "MSF.XETRA", "percentage": "1", "query": "online", "rated": "underrated", "stochastic": "40"}'
+curl -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "'$1'", "percentage": "'$2'", "query": "'$3'", "rated": "'$4'", "stochastic": "'$5'"}'
 #curl -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "MSF.XETRA", "percentage": "1", "query": "online", "rated": "underrated", "stochastic": "40"}'
