157c257ea6ab0c3625feeb06c45f49d963e4483e
Hefezopf <markus@hopf-it.de>
2020-12-06 14:46:12 +0100

    kkk

--- analyse.sh
+++ analyse.sh
@@ -192,19 +192,20 @@ ProgressBar() {
 }
 
 
-echo xxxxxxxxxxxxxxx
-which sh
-		
+	
 stochasticQuoteList=$(echo " , , , , 4, 9, 6, 8,")
 # Revers and output the last x numbers
 stochasticQuoteList=$(echo "$stochasticQuoteList" | awk '{ for(i = length; i!=0; i--) x = x substr($0, i, 1);} END {print x}' | awk -F',' '{ print $1 "," $2 "," $3 "," $4 "," $5 }' )
 
-IFS=',' read -r -a array <<< "$stochasticQuoteList"
+array=$(echo "$stochasticQuoteList" | sed 's/,/\n/g')
+echo jjj "$index ${array[index]}"
+echo kkkk
+#IFS=',' read -r -a array <<< "$stochasticQuoteList"
 for index in "${!array[@]}"
 do
     echo iii "$index ${array[index]}"
 done
-echo mmmmmmmm $stochasticQuoteList
+#echo mmmmmmmm $stochasticQuoteList
 lowValue=8
 inc=0
 #IFS=',' read -ra ADDR <<< "$stochasticQuoteList"
