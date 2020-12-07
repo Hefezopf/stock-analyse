07053c864d0fafa35c373145f275637c796750e6
Hefezopf <markus@hopf-it.de>
2020-12-07 19:00:13 +0100

    hh

--- curl_figi.sh
+++ curl_figi.sh
@@ -2,4 +2,5 @@
 # Call: ./curl_figi.sh SYMBOL
 # Call example: ./curl_figi.sh IBM
 set -x
-curl 'https://api.openfigi.com/v2/mapping' --request POST --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'${1}'"}]' | jq '.[0].data[0].name'
+curl 'https://api.openfigi.com/v2/mapping' --request POST --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'${1}'"}]' | jq '.[0].data[0]'
+#curl 'https://api.openfigi.com/v2/mapping' --request POST --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'${1}'"}]' | jq '.[0].data[0].name'
