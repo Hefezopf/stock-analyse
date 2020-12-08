745e89db5cfed240571bad37102018eb66fcd642
Hefezopf <markus@hopf-it.de>
2020-12-08 22:48:42 +0100

    9

--- curl_github_dispatch.sh
+++ curl_github_dispatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Call: ./curl_github_dispatch.sh SYMBOLS PERCENTAGE QUERY RATED STOCHASTIC
-# Call example: ./curl_github_dispatch.sh "INL.XETRA" 1 offline underrated 20
+# Call example: ./curl_github_dispatch.sh "INL.XETRA" 1 offline underrated 9
 # !! Only one symbol can be passed as parameter!! This is not possible: "INL.XETRA BMW.XETRA" 
 set -x
 curl -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "'$1'", "percentage": "'$2'", "query": "'$3'", "rated": "'$4'", "stochastic": "'$5'"}'
