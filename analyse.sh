58f562702ca011cfdfd92f0b30dfe297cd7b71e5
Hefezopf <markus@hopf-it.de>
2020-12-08 22:45:44 +0100

    ölö

--- analyse.sh
+++ analyse.sh
@@ -211,26 +211,10 @@ do
 		# +Strategie: OverratedByPercentAndStochastic
 		resultStrategieOverratedByPercentAndStochastic=""
 		StrategieOverratedByPercentAndStochastic
-		# if [ "$ratedParam" = 'overrated' ]; then
-		# 	if [ "$lastStochasticQuoteRounded" -gt "$stochasticPercentageUpper" ] && [ "$lastOverAgv18" = 1 ] && [ "$lastOverAgv38" = 1 ] && [ "$lastOverAgv100" = 1 ] && 
-		# 	   [ "$agv18OverAgv38" = 1 ] && [ "$agv38OverAgv100" = 1 ] && [ "$agv18OverAgv100" = 1 ]; then
-		# 		resultStrategieOverratedByPercentAndStochastic="- Overrated: $last EUR is more then $percentageLesserFactor over average18: $average18 EUR and average38: $average38 EUR and over average100: $average100 EUR. Stochastic14 is $lastStochasticQuoteRounded"
-		# 		echo $resultStrategieOverratedByPercentAndStochastic
-		# 		#echo "\"http://www.google.com/search?tbm=fin&q=${symbol}\" " >> $OUT_RESULT_FILE
-		# 	fi
-		# fi
 
 		# -Strategie: UnderratedByPercentAndStochastic
 		resultStrategieUnderratedByPercentAndStochastic=""
 		StrategieUnderratedByPercentAndStochastic
-		# if [ "$ratedParam" = 'underrated' ]; then
-		# 	if [ "$lastStochasticQuoteRounded" -lt "$stochasticPercentageLower" ] && [ "$lastUnderAgv18" = 1 ] && [ "$lastUnderAgv38" = 1 ] && [ "$lastUnderAgv100" = 1 ] && 
-		# 	   [ "$agv18UnderAgv38" = 1 ] && [ "$agv38UnderAgv100" = 1 ] && [ "$agv18UnderAgv100" = 1 ]; then
-		# 		resultStrategieUnderratedByPercentAndStochastic="+ Underrated by percent and stochastic: Last price $last EUR is $percentageGreaterFactor under average18: $average18 EUR and under average38: $average38 EUR and under average100: $average100 EUR and Stochastic14 $lastStochasticQuoteRounded is lower then $stochasticPercentageLower"
-		# 		echo $resultStrategieUnderratedByPercentAndStochastic
-		#         WriteComdirectUrl
-		# 	fi
-		# fi
 	
 	    # -Strategie: Low stochastic 3 last values under lowStochasticValue
 		resultStrategieUnderratedLowStochastic=""
