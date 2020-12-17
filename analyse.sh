c49bd108ae17af76ec01b5169a33ef85ebdddcca
Hefezopf <markus@hopf-it.de>
2020-12-17 19:45:56 +0100

    klöä

--- analyse.sh
+++ analyse.sh
@@ -348,15 +348,15 @@ done
 echo $HTML_RESULT_FILE_END >> $OUT_RESULT_FILE
 
 
-i=1
-for symbolFile in $reportedSymbolFileList
-do
-	echo symbolFile $symbolFile
-	cp $symbolFile out/$i.html
-	i=$(( i + 1 ))
-done
-ls out
-cp js/index_orig.html out
+# i=1
+# for symbolFile in $reportedSymbolFileList
+# do
+# 	echo symbolFile $symbolFile
+# 	cp $symbolFile out/$i.html
+# 	i=$(( i + 1 ))
+# done
+# ls out
+# cp js/index_orig.html out
 
 
 # Time measurement
