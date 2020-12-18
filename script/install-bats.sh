5effc2f392b07b4d33391ddd7a91e81284a4f2ad
Hefezopf <markus@hopf-it.de>
2020-12-18 20:23:53 +0100

    ö

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -3,4 +3,4 @@ clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helpe
 git clone https://github.com/ztombol/bats-core $HOME/test/test_helper/batscore
 git clone https://github.com/ztombol/bats-assert $HOME/test/test_helper/batsassert
 ls $HOME/test/test_helper/batscore
-source $HOME/test/test_helper/batscore/script/install-bats.sh ~
+. $HOME/test/test_helper/batscore/script/install-bats.sh ~
