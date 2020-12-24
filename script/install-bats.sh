47dda874c9cffd1ef4812afe1be12df8465bcc5a
Markus Hopf <markus@hopf-it.de>
2020-12-24 10:09:35 +0100

    Update install-bats.sh

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -2,5 +2,5 @@
 clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helper/batsassert
 git clone https://github.com/ztombol/bats-core $HOME/test/test_helper/batscore
 git clone https://github.com/ztombol/bats-assert $HOME/test/test_helper/batsassert
-ls $HOME/test/test_helper/batscore
+# ls $HOME/test/test_helper/batscore
 . $HOME/test/test_helper/batscore/script/install-bats.sh ~
