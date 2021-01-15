b3ce34c65195c3cda92e4f7fe16308f02b7ec1d0
Hefezopf <markus@hopf-it.de>
2021-01-15 11:54:01 +0100

    key4

---
+++ script/curl/curl_marketstack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Call: ./curl_marketstack.sh SYMBOL
+# Call example: ./script/curl_marketstack.sh IBM
+set -x
+export MARKET_STACK_ACCESS_KEY=$MARKET_STACK_ACCESS_KEY1
+curl -s --location --request GET "http://api.marketstack.com/v1/eod?access_key=${MARKET_STACK_ACCESS_KEY}&exchange=XETRA&symbols=${1}.XETRA" | jq -jr '.data[]|.date, "T", .close, "\n"' | awk -F'T' '{print $1 "\t" $3}'
