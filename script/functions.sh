c7bb4d9997afe9cafcaa5ca561a5a2c4b91041b1
Max Hopf <Markus.Hopf.Extern@fiduciagad.de>
2021-01-20 07:46:27 +0100

    local

--- script/functions.sh
+++ script/functions.sh
@@ -4,9 +4,9 @@
 # Input is _symbolParam($1), _ticker_name_id_file_param($2), _sleepParam($3)
 # Output: -
 CurlSymbolName() {
-    local _symbolParam=${1}
-    local _ticker_name_id_file_param=${2}
-    local _sleepParam=${3}
+    _symbolParam=${1}
+    _ticker_name_id_file_param=${2}
+    _sleepParam=${3}
     symbol=$(echo "${_symbolParam}" | tr '[:lower:]' '[:upper:]')
     symbolName=$(grep -P "$symbol\t" "$_ticker_name_id_file_param" | cut -f 2)
     if [ ! "${#symbolName}" -gt 1 ]; then
@@ -14,7 +14,7 @@ CurlSymbolName() {
 #        symbolName=$(curl -s --location --request POST 'https://api.openfigi.com/v2/mapping' --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'"${_symbolParam}"'"}]' | jq '.[0].data[0].name')
         if ! [ "$symbolName" = 'null' ]; then
             echo "$_symbolParam""$(printf '\t')""$symbolName""$(printf '\t')""999999" | tee -a "$_ticker_name_id_file_param"
-            local temp_ticker_name_id_file
+            temp_ticker_name_id_file
             temp_ticker_name_id_file="$(mktemp -p /dev/shm/)"
             sort -k 1 "$_ticker_name_id_file_param" > "$temp_ticker_name_id_file"
             mv "$temp_ticker_name_id_file" "$_ticker_name_id_file_param"
@@ -28,13 +28,13 @@ CurlSymbolName() {
 # Input is _symbolsParam($1), _percentageParam($2), _queryParam($3), _ratedParam($4), _stochasticPercentageParam($5), _RSIQuoteParam($6), OUT_RESULT_FILE_param($7)
 # Output: OUT_RESULT_FILE
 UsageCheckParameter() {
-    local _symbolsParam=${1}
-    local _percentageParam=${2}
-    local _queryParam=${3}
-    local _ratedParam=${4}
-    local _stochasticPercentageParam=${5}
-    local _RSIQuoteParam=${6}
-    local OUT_RESULT_FILE_param=${7}
+    _symbolsParam=${1}
+    _percentageParam=${2}
+    _queryParam=${3}
+    _ratedParam=${4}
+    _stochasticPercentageParam=${5}
+    _RSIQuoteParam=${6}
+    OUT_RESULT_FILE_param=${7}
 
     if  [ -n "${_symbolsParam##*[!a-zA-Z0-9* ]*}" ] && # symbols, blank and '*' allowed
         [ -n "${_percentageParam##*[!0-9]*}" ]  && 
@@ -70,7 +70,7 @@ UsageCheckParameter() {
 # Input is factor($1), firstCompareValue($2), secondCompareValue($3)
 # Output: 1 if lesser
 LesserThenWithFactor() {
-    local _lesserValue
+    _lesserValue
     _lesserValue=$(echo "$1 $2" | awk '{print $1 * $2}')
     if awk 'BEGIN {exit !('"$_lesserValue"' < '"$3"')}'; then
         return 1
@@ -85,7 +85,7 @@ LesserThenWithFactor() {
 # Example 1.1*100>109 -> return 1
 # Example 1.1*100>110 -> return 0
 GreaterThenWithFactor() {
-    local _greaterValue
+    _greaterValue
     _greaterValue=$(echo "$1 $2" | awk '{print $1 * $2}')
     if awk 'BEGIN {exit !('"$_greaterValue"' > '"$3"')}'; then
         return 1
@@ -98,13 +98,13 @@ GreaterThenWithFactor() {
 # Input is currentStateParam($1) and totalStateParam($2)
 # Output: echo
 ProgressBar() {
-    local currentStateParam=${1}
-    local totalStateParam=${2}
-    local _progress_="$((currentStateParam*10000/totalStateParam))"
-    local _progress=$((_progress_/100))
-    local _done_=$((_progress*4))
-    local _done=$((_done_/10))
-    local _left=$((40-_done))
+    currentStateParam=${1}
+    totalStateParam=${2}
+    _progress_="$((currentStateParam*10000/totalStateParam))"
+    _progress=$((_progress_/100))
+    _done_=$((_progress*4))
+    _done=$((_done_/10))
+    _left=$((40-_done))
     # Build progressbar string lengths
     _fill=$(printf "%${_done}s")
     _empty=$(printf "%${_left}s")                         
@@ -124,13 +124,13 @@ ProgressBar() {
 # Input _out_result_file_param($1), _symbolParam($2), _symbolNameParam($3), _linkColorParam($4), _markerOwnStockParam($5), _reasonParam($6)
 # Output: echo to file
 WriteComdirectUrlAndStoreFileList() {
-    local _out_result_file_param=${1}
-    local _symbolParam=${2}
-    local _symbolNameParam="${3}"
-    local _linkColorParam=${4}
-    local _markerOwnStockParam=${5}
-    local _reasonParam=${6}
-    local id_notation
+    _out_result_file_param=${1}
+    _symbolParam=${2}
+    _symbolNameParam="${3}"
+    _linkColorParam=${4}
+    _markerOwnStockParam=${5}
+    _reasonParam=${6}
+    id_notation
     id_notation=$(grep -P "${symbol}\t" "$TICKER_ID_NAMES_FILE" | cut -f 3)
     if [ ! "${#id_notation}" -gt 1 ]; then
         id_notation=999999
@@ -151,17 +151,17 @@ WriteComdirectUrlAndStoreFileList() {
 # CreateCmdAnalyseHyperlink function:
 # - Write file Hyperlink in CMD, Only works for windows
 CreateCmdAnalyseHyperlink() {
-    local _outputText="# Analyse $symbol $symbolName"
+    _outputText="# Analyse $symbol $symbolName"
     if [ "$(uname)" = 'Linux' ]; then
         echo "$_outputText"
     else
-        local driveLetter
+        driveLetter
         driveLetter=$(pwd | cut -f 2 -d '/')
-        local suffix
+        suffix
         suffix=$(pwd)
         # shellcheck disable=SC3057
-        local suffixPath=${suffix:2:200}
-        local directory=$driveLetter":"$suffixPath
+        suffixPath=${suffix:2:200}
+        directory=$driveLetter":"$suffixPath
         # shellcheck disable=SC3037
         echo -e "\e]8;;file:///""$directory""/out/""$symbol"".html\a$_outputText\e]8;;\a"
     fi
