3599745b30f1641acb5e5d5050ad7db2af655774
Hefezopf <markus@hopf-it.de>
2021-02-09 18:23:51 +0100

    Sell

--- analyse.sh
+++ analyse.sh
@@ -22,9 +22,9 @@
 . ./script/strategies.sh
 
 # Calculate charts and underlying strategies. Default is 'true'
-CalculateStochastic=true
-CalculateRSI=true
-CalculateMACD=true
+# CalculateStochastic=true
+# CalculateRSI=true
+# CalculateMACD=true
 
 # Settings for currency formating like ',' or '.' with 'printf'
 export LC_ALL=en_US.UTF-8
@@ -367,10 +367,8 @@ do
         styleComdirectLink="style=\"font-size:x-large; color:black\""
         # Red link only for stocks that are marked as own stocks
         if [ "${markerOwnStock}" = '*' ] && 
-          { 
-            # TODO
-            [ "${#resultStrategieByTendency}" -gt 1 ] || 
-           # [ "${#resultStrategieOverratedByTendency}" -gt 1 ] || 
+           { 
+           { [ "${#resultStrategieByTendency}" -gt 1 ] &&  [[ $resultStrategieByTendency == *"Sell: "* ]]; } || 
             [ "${#resultStrategieOverratedHighHorizontalMACD}" -gt 1 ] || [ "${#resultStrategieOverratedByPercentAndStochastic}" -gt 1 ] || 
             [ "${#resultStrategieOverratedXHighStochastic}" -gt 1 ] || [ "${#resultStrategieOverratedXHighRSI}" -gt 1 ] || 
             [ "${#resultStrategieOverratedHighStochasticHighRSIHighMACD}" -gt 1 ]; } then
@@ -378,14 +376,13 @@ do
         fi
 
         if 
-           # TODO
-           [ "${#resultStrategieByTendency}" -gt 1 ] || 
-        #[ "${#resultStrategieUnderratedByTendency}" -gt 1 ] || 
+           { [ "${#resultStrategieByTendency}" -gt 1 ] &&  [[ $resultStrategieByTendency == *"Buy: "* ]]; } || 
            [ "${#resultStrategieUnderratedLowHorizontalMACD}" -gt 1 ] || [ "${#resultStrategieUnderratedByPercentAndStochastic}" -gt 1 ] || 
            [ "${#resultStrategieUnderratedXLowStochastic}" -gt 1 ] || [ "${#resultStrategieUnderratedXLowRSI}" -gt 1 ] || 
            [ "${#resultStrategieUnderratedLowStochasticLowRSILowMACD}" -gt 1 ]; then
             styleComdirectLink="style=\"font-size:x-large; color:green\""
         fi
+
         ID_NOTATION=$(grep -P "${symbol}\t" $TICKER_ID_NAMES_FILE | cut -f 3)
         echo "<p><a $styleComdirectLink href=""$COMDIRECT_URL_PREFIX""$ID_NOTATION" " target=_blank>$markerOwnStock$symbol $symbolName</a><br>"
         echo "Percentage:<b>$percentageParam</b> "
