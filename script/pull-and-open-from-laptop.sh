3c09462be56b8870f345ea3f828cf5c7a76f47e4
Max Hopf <Markus.Hopf.Extern@fiduciagad.de>
2021-03-11 08:31:54 +0100

    usa

--- script/pull-and-open-from-laptop.sh
+++ script/pull-and-open-from-laptop.sh
@@ -2,6 +2,7 @@
 
 #alias usa='sh /c/max/sa/script/pull-and-open-from-laptop.sh'
 
+cd /c/max/sa/
 git pull
 sed -i 's/D:\/code\/stock-analyse\/out/C:\/max\/sa\/out/g' /C/max/sa/out/_result.html
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito "C:/max/sa/out/_result.html"
