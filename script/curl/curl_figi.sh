2e0f1548c6f9c72e4a5409055154507639faa2d8
Max Hopf <Markus.Hopf.Extern@fiduciagad.de>
2021-03-13 09:29:20 +0100

    curl

--- script/curl/curl_figi.sh
+++ script/curl/curl_figi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Call: ./curl_figi.sh SYMBOL
-# Call example: ./script/curl_figi.sh IBM
+# Mapping from SYMBOL to Stock name
+# Call: sh ./curl_figi.sh SYMBOL
+# Call example: sh ./script/curl/curl_figi.sh IBM
 set -x
 curl --request POST 'https://api.openfigi.com/v2/mapping' --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'${1}'"}]' | jq '.[0].data[0].name'
