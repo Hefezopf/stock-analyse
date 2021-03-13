5fef18520819b215ae06c24ab11f39429ca7fded
Max Hopf <Markus.Hopf.Extern@fiduciagad.de>
2021-03-13 09:57:15 +0100

    curl

--- script/curl/curl_marketstack.sh
+++ script/curl/curl_marketstack.sh
@@ -5,4 +5,4 @@
 # Call example: sh ./script/curl/curl_marketstack.sh IBM
 set -x
 export MARKET_STACK_ACCESS_KEY=$MARKET_STACK_ACCESS_KEY1
-curl -s --location --request GET "http://api.marketstack.com/v1/eod?access_key=${MARKET_STACK_ACCESS_KEY}&exchange=XNAS&symbols=${1}" | jq -jr '.data[]|.date, "T", .close, "\n"' | awk -F'T' '{print $1 "\t" $3}'
+curl -s --location --request GET "http://api.marketstack.com/v1/eod?access_key=${MARKET_STACK_ACCESS_KEY}&exchange=XETRA&symbols=${1}.XETRA" | jq -jr '.data[]|.date, "T", .close, "\n"' | awk -F'T' '{print $1 "\t" $3}'
