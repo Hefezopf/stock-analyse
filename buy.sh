d44e04871c7c84814a381d476efa95dfe2993151
Hefezopf <markus@hopf-it.de>
2021-04-16 16:18:04 +0200

    echo sell

--- buy.sh
+++ buy.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# Adding *symbol to _own_symbols.txt
+# Adding *symbol to data/_own_symbols.txt
 # and
-# Removing symbol from _stock_symbols.txt
+# Removing symbol from data/_stock_symbols.txt
+# and
+# Adding symbol to data/_buying_rate.txt
 
-# Call: sh ./buy.sh SYMBOL
-# Example: sh ./buy.sh IBM
+# Call: sh ./buy.sh SYMBOL BUYING_RATE
+# Example: sh ./buy.sh IBM 9.99
 
-echo buy ${1} ...
+echo buy ${1} ${2} ...
 
 sed -i "0,/^/s//*${1} /" data/_own_symbols.txt
 sed -i "s/${1} //" data/_stock_symbols.txt
-git add data/_own_symbols.txt data/_stock_symbols.txt
-git commit -m "buy ${1}"
-git push
+
+sed -i '1 i\'${1}' '${2}'' data/_buying_rate.txt
+
+# git add data/_own_symbols.txt data/_stock_symbols.txt data/_buying_rate.txt
+# git commit -m "buy ${1} ${2}"
+# git push
--- sell.sh
+++ sell.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# Removing *symbol from _own_symbols.txt
+# Removing *symbol from data/_own_symbols.txt
 # and
-# Adding symbol to _stock_symbols.txt
+# Adding symbol to data/_stock_symbols.txt
+# and 
+# Removing symbol from data/_buying_rate.txt
 
 # Call: sh ./sell.sh SYMBOL
 # Example: sh ./sell.sh IBM
@@ -11,6 +13,11 @@ echo sell ${1} ...
 
 sed -i "s/*${1} //" data/_own_symbols.txt
 sed -i "0,/^/s//${1} /" data/_stock_symbols.txt
-git add data/_own_symbols.txt data/_stock_symbols.txt
-git commit -m "sell ${1}"
-git push
+
+sed "/^${1} /d" data/_buying_rate.txt > data/_buying_rate_temp.txt
+rm -rf data/_buying_rate.txt
+mv data/_buying_rate_temp.txt data/_buying_rate.txt
+
+#git add data/_own_symbols.txt data/_stock_symbols.txt data/_buying_rate.txt
+#git commit -m "sell ${1}"
+#git push
