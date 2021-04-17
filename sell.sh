35aac8715757c449fa79bad1cdff842beb42c118
Hefezopf <markus@hopf-it.de>
2021-04-17 12:42:30 +0200

    10 min

--- sell.sh
+++ sell.sh
@@ -16,7 +16,7 @@ sed -i "0,/^/s//${1} /" config/stock_symbols.txt
 
 sed "/^${1} /d" config/buying_rate.txt > config/buying_rate_temp.txt
 rm -rf config/buying_rate.txt
-mv config_buying_rate_temp.txt config/buying_rate.txt
+mv config/buying_rate_temp.txt config/buying_rate.txt
 
 #git add config/own_symbols.txt config/stock_symbols.txt config/buying_rate.txt
 #git commit -m "sell ${1}"
