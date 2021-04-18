bccdb1f37eaca2a7c433434e09c8872858ddcf66
Hefezopf <markus@hopf-it.de>
2021-04-18 09:21:43 +0200

    curl

--- script/curl/curl_github_dispatch.sh
+++ script/curl/curl_github_dispatch.sh
@@ -6,4 +6,4 @@
 # Example: sh ./script/curl/curl_github_dispatch.sh "*GIS" 1 offline 9 30
 # !!Only one symbol can be passed as parameter!! For example: This list is NOT possible: "INL BMW" 
 set -x
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "'$1'", "percentage": "'$2'", "query": "'$3'", "stochastic": "'$4'", "RSI": "'$5'"}'
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" -H 'Accept: application/vnd.github.everest-preview+json' "https://api.github.com/repos/Hefezopf/stock-analyse/dispatches" -d '{"event_type": "curl", "client_payload": {"symbols": "'$1'", "percentage": "'$2'", "query": "'$3'", "stochastic": "'$4'", "RSI": "'$5'"}}'
