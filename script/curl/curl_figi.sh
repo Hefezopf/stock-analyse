2e8da2d927b2e7e269ae2b21ea9780a141750aeb
Hefezopf <markus@hopf-it.de>
2021-04-18 09:46:37 +0200

    parameters

--- script/curl/curl_figi.sh
+++ script/curl/curl_figi.sh
@@ -3,5 +3,11 @@
 # Request a real Stock name for a given SYMBOL name
 # Call: sh ./curl_figi.sh SYMBOL
 # Example: sh ./script/curl/curl_figi.sh IBM
+
+if { [ -z "$1" ]; } then
+  echo "Not all parameters specified!"
+  echo "Example: curl_figi.sh IBM"
+  exit 1
+fi
 set -x
 curl --request POST 'https://api.openfigi.com/v2/mapping' --header 'Content-Type: application/json' --header 'echo ${X_OPENFIGI_APIKEY}' --data '[{"idType":"TICKER", "idValue":"'${1}'"}]' | jq '.[0].data[0].name'
