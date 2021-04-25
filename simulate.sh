f4be60fdb1924c1409a3d124336b94ef17f20934
Hefezopf <markus@hopf-it.de>
2021-04-25 12:38:54 +0200

    sim

--- simulate.sh
+++ simulate.sh
@@ -16,7 +16,7 @@ RSI_sellLevelParam=$4
 
 HISTORY_FILE=history/"$symbolParam".txt
 wallet=0
-pieces=0
+piecesInWallet=0
 
 historyQuotes=$(head -n2 "$HISTORY_FILE" | tail -1)
 echo "$historyQuotes"
@@ -31,23 +31,23 @@ MACDindex=0
 for valueMACD in $(echo "$historyRSIs" | sed "s/,/ /g")
 do
     if [ "$valueMACD" -lt "$RSI_buyLevelParam" ]; then
-        #echo buying...
         quoteAt="$(echo "$historyQuotes" | cut -f "$MACDindex" -d ',')" 
-        amount=$(echo "$quoteAt 100" | awk '{print ($1 * $2)}')
-        pieces=$(echo "$pieces 100" | awk '{print ($1 + $2)}')
+        piecesPerTrade=$(echo "$amountPerTradeParam $quoteAt" | awk '{print ($1 / $2)}')
+        piecesPerTrade=${piecesPerTrade%.*}
+        amount=$(echo "$quoteAt $piecesPerTrade" | awk '{print ($1 * $2)}')
+        piecesInWallet=$(echo "$piecesInWallet $piecesPerTrade" | awk '{print ($1 + $2)}')
         wallet=$(echo "$wallet $amount" | awk '{print ($1 + $2)}')
-        echo buy 100 pieces: MACD:"$valueMACD" at positon:"$MACDindex" Quote:"$quoteAt"€ Amount="$amount"€ Wallet=$wallet€
+        echo buy $piecesPerTrade pieces: MACD:"$valueMACD" at positon:"$MACDindex" Quote:"$quoteAt"€ Amount="$amount"€ piecesInWallet=$piecesInWallet Wallet=$wallet€
     fi
-    if [ "$wallet" -gt 0 ] && [ "$valueMACD" -gt "$RSI_sellLevelParam" ]; then
-        #echo selling...
+    if [ "${wallet%.*}" -gt 0 ] && [ "$valueMACD" -gt "$RSI_sellLevelParam" ]; then
         quoteAt="$(echo "$historyQuotes" | cut -f "$MACDindex" -d ',')" 
-        amount=$(echo "$quoteAt $pieces" | awk '{print ($1 * $2)}')
-        echo sell $pieces pieces: MACD:"$valueMACD" at positon:"$MACDindex" Quote:"$quoteAt"€ Amount="$amount"€
-        pieces=0
+        amount=$(echo "$quoteAt $piecesInWallet" | awk '{print ($1 * $2)}')
+        echo sell $piecesInWallet piecesInWallet: MACD:"$valueMACD" at positon:"$MACDindex" Quote:"$quoteAt"€ Amount="$amount"€
+        piecesInWallet=0
         wallet=$(echo "$amount $wallet" | awk '{print ($1 - $2)}')
     fi
     MACDindex=$((MACDindex + 1))
 done
 echo "-----------"
 echo wallet=$wallet€
-echo pieces=$pieces
+echo piecesInWallet=$piecesInWallet
