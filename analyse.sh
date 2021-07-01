82556edb795363a86a0989fa7dadbd2cac8a7fc9
Markus Hopf <Markus.Hopf.Extern@fiduciagad.de>
2021-07-01 13:59:57 +0200

    media

--- analyse.sh
+++ analyse.sh
@@ -76,24 +76,28 @@ HTML_RESULT_FILE_HEADER="<!DOCTYPE html><html lang=\"en\"><head>
 <title>Result SA</title>
 <style>.green{color:green;} .red{color:red;} .black{color:black;}
  /* iphone 3 */
-@media only screen and (min-device-width: 320px) and (max-device-height: 480px) and (-webkit-device-pixel-ratio: 1) { body > div {
+@media only screen and (min-device-width: 320px) and (max-device-height: 480px) and (-webkit-device-pixel-ratio: 1) {
+body > div {
     font-size: xx-large;
-  }}
+}}
         
 /* iphone 4 */
-@media only screen and (min-device-width: 320px) and (max-device-height: 480px) and (-webkit-device-pixel-ratio: 2) { body > div {
+@media only screen and (min-device-width: 320px) and (max-device-height: 480px) and (-webkit-device-pixel-ratio: 2) {
+body > div {
     font-size: xx-large;
-  }}
+}}
 
 /* iphone 5 */
-@media only screen and (min-device-width: 320px) and (max-device-height: 568px) and (-webkit-device-pixel-ratio: 2) {body > div {
+@media only screen and (min-device-width: 320px) and (max-device-height: 568px) and (-webkit-device-pixel-ratio: 2) {
+body > div {
     font-size: xx-large;
-  }}
+}}
 
 /* iphone 6, 6s, 7, 8 */
-@media only screen and (min-device-width: 375px) and (max-device-height: 667px) and (-webkit-device-pixel-ratio: 2) { body > div {
+@media only screen and (min-device-width: 375px) and (max-device-height: 667px) and (-webkit-device-pixel-ratio: 2) {
+    body > div {
     font-size: xx-large;
-  }}
+}}
     
 /* iphone 6+, 6s+, 7+, 8+ */
 @media only screen and (min-device-width: 414px) and (max-device-height: 736px) and (-webkit-device-pixel-ratio: 3) { body > div {
@@ -101,14 +105,16 @@ HTML_RESULT_FILE_HEADER="<!DOCTYPE html><html lang=\"en\"><head>
   }}
 
 /* iphone X , XS, 11 Pro, 12 Mini */
-@media only screen and (min-device-width: 375px) and (max-device-height: 812px) and (-webkit-device-pixel-ratio: 3) { body > div {
+@media only screen and (min-device-width: 375px) and (max-device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+    body > div {
     font-size: xx-large;
-  }}
+}}
 
 /* iphone 12, 12 Pro */
-@media only screen and (min-device-width: 390px) and (max-device-height: 844px) and (-webkit-device-pixel-ratio: 3) { body > div {
+@media only screen and (min-device-width: 390px) and (max-device-height: 844px) and (-webkit-device-pixel-ratio: 3) {
+    body > div {
     font-size: x-large;
-  }}
+}}
 
 /* iphone XR, 11 */
 @media only screen and (min-device-width : 414px) and (max-device-height : 896px) and (-webkit-device-pixel-ratio : 2) {body > div {
@@ -116,14 +122,16 @@ HTML_RESULT_FILE_HEADER="<!DOCTYPE html><html lang=\"en\"><head>
   } }
     
 /* iphone XS Max, 11 Pro Max */
-@media only screen and (min-device-width : 414px) and (max-device-height : 896px) and (-webkit-device-pixel-ratio : 3) {body > div {
+@media only screen and (min-device-width : 414px) and (max-device-height : 896px) and (-webkit-device-pixel-ratio : 3) {
+    body > div {
     font-size: x-large;
-  }}
+}}
 
 /* iphone 12 Pro Max */
-@media only screen and (min-device-width : 428px) and (max-device-height : 926px) and (-webkit-device-pixel-ratio : 3) {body > div {
+@media only screen and (min-device-width : 428px) and (max-device-height : 926px) and (-webkit-device-pixel-ratio : 3) {
+    body > div {
     font-size: x-large;
-  }}
+}}
 </style>
 </head><body>
 <div>
