8c0301263d3a5fd27c395ec8c094a34b32a1c552
Hefezopf <markus@hopf-it.de>
2021-11-07 10:08:03 +0100

    _simulate.html

--- simulate.sh
+++ simulate.sh
@@ -9,7 +9,7 @@
 # 5. Parameter: INCREMENT_PER_TRADE: Factor how many more stock to buy on each subsequent order: like 1.1 mean 10% more.
 # Call example: ./simulate.sh 'BEI ALV' 2000 25 91 1.1
 # Call example: ./simulate.sh 'BEI ALV' 2500 10 96 1.01
-# Grep output to find symbols: grep Position:100 out/_simulate.txt
+# Grep output to find symbols: grep Position:100 out/_simulate.html
 
 # Debug mode
 #set -x
