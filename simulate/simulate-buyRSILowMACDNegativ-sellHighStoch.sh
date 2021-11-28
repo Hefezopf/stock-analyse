c5afb0112550a537cc228c73618fdef9af14cff7
Markus Hopf <info@hopf-it.de>
2021-11-28 12:48:54 +0100

    tests

--- simulate/simulate-buyRSILowMACDNegativ-sellHighStoch.sh
+++ simulate/simulate-buyRSILowMACDNegativ-sellHighStoch.sh
@@ -226,10 +226,11 @@ do
     cp out/"$symbol".html simulate/out/"$symbol".html
     xAxis="'14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99'"
     for i in "${!ARRAY_TX_INDEX[@]}"; do
-        Out "$i Tx:${ARRAY_TX_INDEX[i]}" $OUT_SIMULATE_FILE
-        xAxis=$(echo "$xAxis" | sed "s/"$i"/"${ARRAY_TX_INDEX[i]}"/g")
+        # Out "$i Tx:${ARRAY_TX_INDEX[i]}" $OUT_SIMULATE_FILE
+        # shellcheck disable=SC2001
+        xAxis=$(echo "$xAxis" | sed "s/$i/${ARRAY_TX_INDEX[i]}/g")
     done
-    sed -i "/labels: /c\labels: ["$xAxis"" simulate/out/"$symbol".html
+    sed -i "/labels: /c\labels: [$xAxis" simulate/out/"$symbol".html
 
 done
 
