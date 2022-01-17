3feaf1aa93ebcb5007e35126c08bf5f8a7981300
Markus Hopf <info@hopf-it.de>
2022-01-17 07:14:14 +0100

    sort

---
+++ script/sort_sa.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Alphabetic sorting of all Symbols in config/stock_symbols.txt
+# Do manually every other month, to sort all available symbols again.
+
+# Call: sh ./sort_sa.sh
+
+echo "Sorting..."
+
+# Delete $SYMBOL in stock_symbols.txt
+cat ../config/stock_symbols.txt | tr " " "\n" | sort | tr "\n" " " > ../config/stock_symbols.txt
