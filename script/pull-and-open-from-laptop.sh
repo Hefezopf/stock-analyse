2d20f8fe186dcb9ea629005937e6e4c4a7b4adbb
Markus Hopf <Markus.Hopf.Extern@atruvia.de>
2022-02-15 10:07:52 +0100

    script

--- script/pull-and-open-from-laptop.sh
+++ script/pull-and-open-from-laptop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#alias usa='sh /c/max/sa/script/pull-and-open-from-laptop.sh'
+#alias usa='sh /c/code/stock-analyse/script/pull-and-open-from-laptop.sh'
 
-cd /c/max/sa/
+cd /c/code/stock-analyse/
 git pull
-sed -i 's/D:\/code\/stock-analyse\/out/file:\/\/\/C:\/max\/sa\/out/g' /C/max/sa/out/_result.html
-#"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito "C:/max/sa/out/_result.html"
-start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -private -url "file:///C:/max/sa/out/_result.html"
+sed -i 's/D:\/code\/stock-analyse\/out/file:\/\/\/C:\/code\/stock-analyse\/out/g' /C/code/stock-analyse/out/_result.html
+#"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito "C:/code/stock-analyse/out/_result.html"
+start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -private -url "file:///C:/code/stock-analyse/out/_result.html"
