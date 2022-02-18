312044593378bd066f7768565015e54b9e3f8a2a
Markus Hopf <markus.hopf.extern@atruvia.de>
2022-02-18 09:42:21 +0100

    chrome

--- script/pull-and-open-from-laptop.sh
+++ script/pull-and-open-from-laptop.sh
@@ -5,5 +5,5 @@
 cd /c/code/stock-analyse/
 git pull
 sed -i 's/D:\/code\/stock-analyse\/out/file:\/\/\/C:\/code\/stock-analyse\/out/g' /C/code/stock-analyse/out/_result.html
-#"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito "C:/code/stock-analyse/out/_result.html"
-start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -private -url "file:///C:/code/stock-analyse/out/_result.html"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --incognito "C:/code/stock-analyse/out/_result.html"
+#start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -private -url "file:///C:/code/stock-analyse/out/_result.html"
