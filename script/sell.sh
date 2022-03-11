65eb75c76113f527e2da4798ed53d25e7049bdf9
Markus Hopf <markus.hopf.extern@atruvia.de>
2022-03-11 17:08:48 +0100

    autoclf

--- script/sell.sh
+++ script/sell.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # Removing *symbol from config/own_symbols.txt
 # and
 # Adding symbol to config/stock_symbols.txt
@@ -6,14 +7,17 @@
 # Example: sh ./script/sell.sh BEI
 # alias sell='/d/code/stock-analyse/script/sell.sh $1'
 # {"event_type": "sell", "client_payload": {"symbol": "BEI"}}
+
 # To uppercase
 symbolParam=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 echo "Sell $symbolParam"
-#if { [ -z "$symbolParam" ]; } then
-#  echo "Not all parameters specified!"
-#  echo "Example: curl_github_dispatch_sell.sh BEI"
-#  exit 1
-#fi
+
+if { [ -z "$symbolParam" ]; } then
+  echo "Not all parameters specified!"
+  echo "Example: curl_github_dispatch_sell.sh BEI"
+  exit 1
+fi
+
 # Add symbol in front of overall list
 sed -i "0,/^/s//$symbolParam /" config/stock_symbols.txt
 # Encrypt
