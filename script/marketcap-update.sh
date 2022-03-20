959e2a7052fd178e7690d1e2e462a8d29def7233
Markus Hopf <info@hopf-it.de>
2022-03-20 12:04:26 +0100

    ###

--- script/marketcap-update.sh
+++ script/marketcap-update.sh
@@ -31,22 +31,19 @@ do
   marktkap=$(curl -s --location --request GET "https://www.comdirect.de/inf/aktien/detail/uebersicht.html?ID_NOTATION=$ID_NOTATION" | grep -m 1 "#160;Mrd.&nbsp;EUR<" | grep -o '>.*' | cut -f1 -d"," | cut -c 2-)
   if [ "$marktkap" ]; then
     echo "$symbol $marktkap Mrd."
-    # Replace till end of line: idempotent!
-    #sed -i "s/$ID_NOTATION.*/$ID_NOTATION\t$marktkap/g" "$TICKER_NAME_ID_FILE"
   else
     # Bil.
     marktkap=$(curl -s --location --request GET "https://www.comdirect.de/inf/aktien/detail/uebersicht.html?ID_NOTATION=$ID_NOTATION" | grep -m 1 "#160;Bil.&nbsp;EUR<" | grep -o '>.*' | cut -f1 -d"," | cut -c 2-)
     if [ "$marktkap" ]; then
         marktkap=$(echo "$marktkap"000"")
         echo "$symbol $marktkap Mrd."
-        # Replace till end of line: idempotent!
-       # sed -i "s/$ID_NOTATION.*/$ID_NOTATION\t$marktkap"000"/g" "$TICKER_NAME_ID_FILE"
     else
-        echo "ERROR: $symbol $ID_NOTATION= ETF or market cap too small! $marktkap"
         marktkap="?"
         errorSymbols=$(echo "$symbol $errorSymbols")
+        echo "ERROR: $symbol $ID_NOTATION= ETF or market cap too small! $marktkap"
     fi
   fi
+  # Replace till end of line: idempotent!
   sed -i "s/$ID_NOTATION.*/$ID_NOTATION\t$marktkap/g" "$TICKER_NAME_ID_FILE"
 done
 
