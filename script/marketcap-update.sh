228ce9ca5a74b795a52bd612bfa0c0d7f260dd06
Markus Hopf <info@hopf-it.de>
2022-03-23 20:26:08 +0100

    div ohne ,

--- script/marketcap-update.sh
+++ script/marketcap-update.sh
@@ -84,7 +84,7 @@ do
   sed -i "s/$ID_NOTATION.*/$ID_NOTATION\t$marktkap\t$branche\t$kgve/g" "$TICKER_NAME_ID_FILE"
 
   # DIVe
-  dive=$(echo "$curlResponse" | grep -A1 ">DIVe<" | tail -n 1 | cut -f2 -d"<" | cut -f1 -d"&"  | cut -c 4-)
+  dive=$(echo "$curlResponse" | grep -A1 ">DIVe<" | tail -n 1 | cut -f2 -d"<" | cut -f1 -d","  | cut -c 4-)
   if [ "$dive" ]; then
       # Replace ',' with '.'
       dive=$(echo $dive | sed "s/,/./g")  
