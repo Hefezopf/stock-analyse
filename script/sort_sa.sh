39bcb0f241251d2e29606f1ed3ad5f2aab263483
Markus Hopf <info@hopf-it.de>
2022-04-01 12:29:45 +0200

    02M

--- script/sort_sa.sh
+++ script/sort_sa.sh
@@ -6,32 +6,12 @@
 
 # Call: sh ./script/sort_sa.sh
 
-set -x
+#set -x
 
 echo "Sorting..."
-
-
-cat config/stock_symbols.txt 
-echo "Sorting...1"
-cat config/stock_symbols.txt | tr " " "\n" | 
-echo "Sorting...2"
-cat config/stock_symbols.txt | tr " " "\n" | sort
-echo "Sorting...3"
-cat config/stock_symbols.txt | tr " " "\n" | sort | tr "\n" " "
-echo "Sorting...4"
 
 # Sort symbols in stock_symbols.txt
 cat config/stock_symbols.txt | tr " " "\n" | sort | tr "\n" " " > config/stock_symbols_temp.txt
 
-
-
-
-ls config/stock_symbols_temp.txt -lisa
-cat config/stock_symbols_temp.txt
-
-
 rm config/stock_symbols.txt
 mv config/stock_symbols_temp.txt config/stock_symbols.txt
-
-ls config/stock_symbols.txt -lisa
-cat config/stock_symbols.txt
