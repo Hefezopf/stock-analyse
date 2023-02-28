b8e56d64ca7ac5a3668f412a6ace27951fb9bc30
Markus Hopf <info@hopf-it.de>
2023-02-28 17:53:34 +0100

    --branch=v1.8.2

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -5,5 +5,5 @@ export TERM=xterm # Solved warning in github action
 clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helper/batsassert
 
 #git clone https://github.com/bats-core/bats-core.git/tags/v1.8.2 $HOME/test/test_helper/batscore
-git clone https://github.com/bats-core/bats-core.git --branch 1.8.2 $HOME/test/test_helper/batscore
+git clone https://github.com/bats-core/bats-core.git --branch=v1.8.2 $HOME/test/test_helper/batscore
 git clone https://github.com/bats-core/bats-assert.git $HOME/test/test_helper/batsassert
