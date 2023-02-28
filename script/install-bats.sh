60cc8edaa7e7f4099e8ccf371b44e9827bf0ddd0
Markus Hopf <info@hopf-it.de>
2023-02-28 17:30:32 +0100

    which bats

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -5,16 +5,6 @@ clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helpe
 git clone https://github.com/bats-core/bats-core.git $HOME/test/test_helper/batscore
 git clone https://github.com/bats-core/bats-assert.git $HOME/test/test_helper/batsassert
 
-#ls -lias $HOME/test/test_helper/batscore/install.sh
-#chmod +x $HOME/test/test_helper/batscore/install.sh
-
-#. $HOME/test/test_helper/batscore/install.sh ~
 . $HOME/test/test_helper/batscore/bin/bats -v
 
-#echo $HOME
-
-#ls /home/runner/test/test_helper
-
-#echo xxxxxxxxxxx
-
-#pwd
+which bats
