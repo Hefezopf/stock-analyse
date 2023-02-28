87b5964602a3bd3edba860f6691529db755bfd0a
Markus Hopf <info@hopf-it.de>
2023-02-28 17:08:24 +0100

    ls $HOME

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -3,4 +3,7 @@ export TERM=xterm # Solved warning in github action
 clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helper/batsassert
 git clone https://github.com/bats-core/bats-core.git $HOME/test/test_helper/batscore
 git clone https://github.com/bats-core/bats-assert.git $HOME/test/test_helper/batsassert
+
+ls $HOME/test/test_helper/batscore
+
 . $HOME/test/test_helper/batscore/install.sh ~
