5b04cd5532aa66e11fa3f7d1ef81bb41c820728c
Markus Hopf <info@hopf-it.de>
2023-05-14 15:12:45 +0200

    ./temp/data

--- script/curl/curl_getInformerData.sh
+++ script/curl/curl_getInformerData.sh
@@ -23,8 +23,8 @@ do
     value=$(echo "$curlResponse" | grep -m1 "&nbsp;EUR<" | grep -o 'medium.*' | cut -f1 -d"<" | cut -c 9-)
     echo "---------------"
     if [ "$value" ]; then
-        echo "Symbol:$symbol ID_NOTATION:$ID_NOTATION" > "../../temp/data$symbol.txt"
-        echo "Date: $date Value:$value €" > "../../temp/data$symbol.txt"
+        echo "Symbol:$symbol ID_NOTATION:$ID_NOTATION" > "./temp/data$symbol.txt"
+        echo "Date: $date Value:$value €" > "./temp/data$symbol.txt"
 
     else
         echo "Error retrieving Value"
