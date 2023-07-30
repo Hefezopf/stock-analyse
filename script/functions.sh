4ea2571f612a73a10de00f62f3d788a79f396861
Markus Hopf <info@hopf-it.de>
2023-07-30 11:24:27 +0200

    border

--- script/functions.sh
+++ script/functions.sh
@@ -273,9 +273,8 @@ WriteComdirectUrlAndStoreFileList() {
     if [ ! "$_id_notation" = "$ID_NOTATION_STORE_FOR_NEXT_TIME" ]; then
         ID_NOTATION_STORE_FOR_NEXT_TIME=$_id_notation
         {
-            # Hover Chart           
-            echo "<img border='1' id='imgToReplace$_symbolParam' alt='' loading='lazy' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);' src='https://charts.comdirect.de/charts/rebrush/design_big.chart?AVG1=95&AVG2=38&AVG3=18&AVGTYPE=simple&IND0=SST&IND1=RSI&IND2=MACD&LCOLORS=5F696E&TYPE=MOUNTAIN&LNOTATIONS=$_id_notation&TIME_SPAN=10D'/>"
-            #echo "<img border='1' id='imgToReplace$_symbolParam' alt='' loading='lazy' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);' src='https://charts.comdirect.de/charts/rebrush/design_big.chart?AVG1=95&AVG2=38&AVG3=18&AVGTYPE=simple&IND0=SST&IND1=RSI&IND2=MACD&LCOLORS=5F696E&TYPE=MOUNTAIN&LNOTATIONS=$_id_notation&TIME_SPAN=10D'/>"
+            # Hover Chart
+            echo "<img class='imgborder' id='imgToReplace$_symbolParam' alt='' loading='lazy' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);' src='https://charts.comdirect.de/charts/rebrush/design_big.chart?AVG1=95&AVG2=38&AVG3=18&AVGTYPE=simple&IND0=SST&IND1=RSI&IND2=MACD&LCOLORS=5F696E&TYPE=MOUNTAIN&LNOTATIONS=$_id_notation&TIME_SPAN=10D'/>"
             echo "<div style='margin-top: 6px'>"
             echo "<a id='headlineLink$_symbolParam' style='color:$_linkColorParam' onmouseover=\"javascript:showChartImage('10D', '$_symbolParam')\" onmouseout=\"javascript:hideChartImage('$_symbolParam')\" href='$COMDIRECT_URL_PREFIX_10D$_id_notation' target='_blank'>$_markerOwnStockParam$_symbolParam $_symbolNameParam</a>"
             echo "<a style='color:$_linkColorParam' onmouseover=\"javascript:showChartImage('6M', '$_symbolParam')\" onmouseout=\"javascript:hideChartImage('$_symbolParam')\" href='$COMDIRECT_URL_PREFIX_6M$_id_notation' target='_blank'>&nbsp;6M&nbsp;</a>"
@@ -287,8 +286,8 @@ WriteComdirectUrlAndStoreFileList() {
     fi
     # Show reason in result file only, if marked as own stock or a 'buy' recommendation
     if [ "$_markerOwnStockParam" = '*' ] || [ "$_linkColorParam" = "$GREEN" ]; then
-#        echo "<span>$_reasonParam<br></span>" >> "$_outResultFileParam"
-        echo "<span id='detailsIdReason'>$_reasonParam<br></span>" >> "$_outResultFileParam"
+        uniqueId=$(echo "$_symbolParam$_reasonParam" | tr -d '[:space:]')
+        echo "<span id='detailsIdReason$uniqueId'>$_reasonParam<br></span>" >> "$_outResultFileParam"
     fi
 }
 
