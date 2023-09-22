49d2de9c8fe1263a7aeec2e929604db610ed325d
Markus Hopf <markus.hopf@external.t-systems.com>
2023-09-22 12:11:17 +0200

    50%

--- script/functions.sh
+++ script/functions.sh
@@ -274,7 +274,7 @@ WriteComdirectUrlAndStoreFileList() {
         ID_NOTATION_STORE_FOR_NEXT_TIME=$_id_notation
         {
             # Hover Chart
-            echo "<img class='imgborder' id='imgToReplace$_symbolParam' alt='' loading='lazy' style='display:none;position:fixed;top:80%;left:50%;transform:translate(-50%,-50%);width:60%;' src='https://charts.comdirect.de/charts/rebrush/design_big.chart?AVG1=95&AVG2=38&AVG3=18&AVGTYPE=simple&IND0=SST&IND1=RSI&IND2=MACD&LCOLORS=5F696E&TYPE=MOUNTAIN&LNOTATIONS=$_id_notation&TIME_SPAN=10D'/>"
+            echo "<img class='imgborder' id='imgToReplace$_symbolParam' alt='' loading='lazy' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);' src='https://charts.comdirect.de/charts/rebrush/design_big.chart?AVG1=95&AVG2=38&AVG3=18&AVGTYPE=simple&IND0=SST&IND1=RSI&IND2=MACD&LCOLORS=5F696E&TYPE=MOUNTAIN&LNOTATIONS=$_id_notation&TIME_SPAN=10D'/>"
             echo "<div style='margin-top: 6px'>"
             echo "<a id='headlineLink$_symbolParam' style='color:$_linkColorParam' onmouseover=\"javascript:showChart('10D', '$_symbolParam')\" onmouseout=\"javascript:hideChart('$_symbolParam')\" href='$COMDIRECT_URL_PREFIX_10D$_id_notation' target='_blank'>$_markerOwnStockParam$_symbolParam $_symbolNameParam</a>"
             echo "<a style='color:$_linkColorParam' onmouseover=\"javascript:showChart('6M', '$_symbolParam')\" onmouseout=\"javascript:hideChart('$_symbolParam')\" href='$COMDIRECT_URL_PREFIX_6M$_id_notation' target='_blank'>&nbsp;6M&nbsp;</a>"
