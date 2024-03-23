da57a11793f60e7c2cf5d84b6877a7451af8a747
Markus Hopf <info@hopf-it.de>
2024-03-23 09:01:23 +0100

    sommerz

--- simulate/simulate-buyLowMACDNegativ-sellHighStoch.sh
+++ simulate/simulate-buyLowMACDNegativ-sellHighStoch.sh
@@ -433,8 +433,8 @@ do
     # Write/Replace timestamp. Replace line!
     creationDate=$(date +"%e-%b-%Y %R") # 29-Apr-2021 08:52
     if [ "$(uname)" = 'Linux' ]; then
-      creationDate=$(TZ=EST-1EDT date +"%e-%b-%Y %R") # +2h Winterzeit / Wintertime
-    # creationDate=$(TZ=EST-0EDT date +"%e-%b-%Y %R") # +1h Sommerzeit / Summertime        
+     # creationDate=$(TZ=EST-1EDT date +"%e-%b-%Y %R") # +2h Winterzeit / Wintertime
+     creationDate=$(TZ=EST-0EDT date +"%e-%b-%Y %R") # +1h Sommerzeit / Summertime        
     fi    
     # GOOD_LUCK="<p style='text-align: left; padding-right: 50px'>Good Luck! $creationDate</p>"
     GOOD_LUCK="<br>Good Luck! $creationDate"
