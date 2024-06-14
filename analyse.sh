8526097fadc87cd6a7b5876f31a2186cf7239b3b
Markus Hopf <info@hopf-it.de>
2024-06-14 10:24:26 +0200

    Comdirect Informer

--- analyse.sh
+++ analyse.sh
@@ -876,9 +876,12 @@ fi
 
     # Workflow        
     echo "<br><br># Workflow<br><a href=\"https://github.com/Hefezopf/stock-analyse/actions\" target=\"_blank\">Github Action</a><br>"
+    # Result        
     echo "<br># Result<br><a href=\"https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/_result_schedule.html\" target=\"_blank\">Result Schedule SA</a>"
     echo "<br><a href=\"https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/_result.html\" target=\"_blank\">Result&nbsp;SA</a>"
     echo "<br><a href=\"https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/_simulate.html\" target=\"_blank\">Simulation</a><br>"
+    # Informer        
+    echo "<br><br># Informer<br><a href=\"https://nutzer.comdirect.de/inf/musterdepot/pmd/meineuebersicht.html?name=Max\" target=\"_blank\">Comdirect Informer</a><br>"
     echo "</div>" # END portfolioValueDaxFooterId
     echo "$HTML_RESULT_FILE_END" 
 } >> "$OUT_RESULT_FILE"
