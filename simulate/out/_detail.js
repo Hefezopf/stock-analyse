787d702106b9214acfb01411fad69bd8b8ae461c
Markus Hopf <info@hopf-it.de>
2024-09-16 14:36:45 +0000

    Github push

--- simulate/out/_detail.js
+++ simulate/out/_detail.js
@@ -2,7 +2,7 @@
 function showChart(timeSpan) {
     var elementSpanToReplace = document.getElementById('imgToReplace');
     elementSpanToReplace.style.display = 'block';
-    elementSpanToReplace.style.left = '20%'; 
+    elementSpanToReplace.style.left = '30%'; 
     elementSpanToReplace.style.transform = 'scale(1.8)';
  
     elementSpanToReplace.src = elementSpanToReplace.src + '&TIME_SPAN=' + timeSpan; // Concat is not clean, but works!  
