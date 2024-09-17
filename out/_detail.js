a539f963c1c8ee2917b30b190e85ea0f347a9e3c
Markus Hopf <info@hopf-it.de>
2024-09-17 16:39:49 +0200

    ddddddddddddd

--- out/_detail.js
+++ out/_detail.js
@@ -10,46 +10,7 @@ function curlBuy(symbolParam, price, pieces) {
 }
 
 function curlSell(symbolParam, stockPiecesParam, sellPriceParam) {
-if (stockPiecesParam === '?' && symbolParam.charAt(0) !== '*') {
-alert('Error: Stock ' + symbolParam.trim() + ' not in portfolio!');
-return;
-}
-symbolParam = trimOwnChar(symbolParam);    
 
-if (sellPriceParam == '') {
-alert('Error: Price not set!');
-return;
-}
-
-var headlineLink = symbolParam;
-var headlineLinkElem = document.getElementById('headlineLink' + symbolParam);
-if(headlineLinkElem) {
-headlineLink = headlineLinkElem.innerHTML;
-}
-
-if (confirm('Sell ALL ' + stockPiecesParam + ' pieces of: ' + headlineLink + ' for ' + sellPriceParam + '€?') == false) {
-return;
-}
-var url = 'https://api.github.com/repos/Hefezopf/stock-analyse/dispatches';
-var xhr = new XMLHttpRequest();
-xhr.open('POST', url);
-xhr.setRequestHeader('Authorization', 'token ' + token1 + token2.split("").reverse().join(""));
-xhr.setRequestHeader('Accept', 'application/vnd.github.everest-preview+json');
-xhr.onreadystatechange = function () {
-const DONE = 4; // readyState 4 means the request is done.
-if (xhr.readyState === DONE) {
-console.log(xhr.status);
-console.log(xhr.responseText);
-}
-};
-var data = {
-event_type: 'sell',
-client_payload: {
-symbol: symbolParam,
-sellPrice: sellPriceParam
-}
-};
-xhr.send(JSON.stringify(data));
 }
 
 function trimOwnChar(text) {
