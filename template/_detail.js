8b5a9dcd4318c58d1b09343391a3ad7d382132d2
Markus Hopf <info@hopf-it.de>
2024-09-18 12:09:12 +0200

    <!-- <script type='text/javascript' src='_detail.js' async> -->

--- template/_detail.js
+++ template/_detail.js
@@ -13,11 +13,8 @@ function showChart(timeSpan) {
     else{
         elementSpanToReplace.style.top = '38%';
         elementSpanToReplace.style.left = '38%';
-        // top:25%;left:20%;transform:scale(1.5);'/>"
     }   
     elementSpanToReplace.style.transform = 'scale(1.85)';
-    // elementSpanToReplace.style.left = '26%';
-    // elementSpanToReplace.style.transform = 'scale(1.85)';
  
     // Concat is not clean, but works!
     elementSpanToReplace.src = elementSpanToReplace.src + '&TIME_SPAN=' + timeSpan;
