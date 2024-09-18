3d9a87c31d61423400b68f19d17691bbb0424e3a
Markus Hopf <info@hopf-it.de>
2024-09-18 10:35:18 +0000

    Github push

--- simulate/out/_detail.js
+++ simulate/out/_detail.js
@@ -168,11 +168,8 @@ elementSpanToReplace.style.left = '25%';
 else{
 elementSpanToReplace.style.top = '38%';
 elementSpanToReplace.style.left = '38%';
-// top:25%;left:20%;transform:scale(1.5);'/>"
 }   
 elementSpanToReplace.style.transform = 'scale(1.85)';
-// elementSpanToReplace.style.left = '26%';
-// elementSpanToReplace.style.transform = 'scale(1.85)';
 
 // Concat is not clean, but works!
 elementSpanToReplace.src = elementSpanToReplace.src + '&TIME_SPAN=' + timeSpan;
