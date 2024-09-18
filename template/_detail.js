596f169aa5e15da4a1deceaa7e8e8574a6c4c605
Markus Hopf <info@hopf-it.de>
2024-09-18 14:44:57 +0200

    elementSpanToReplace.style.left = '52%';

--- template/_detail.js
+++ template/_detail.js
@@ -4,20 +4,16 @@
 
 function showChart(timeSpan) {
     var elementSpanToReplace = document.getElementById('imgToReplace');
-    elementSpanToReplace.style.display = 'block';
-
-    //var bodyId = document.getElementById('bodyId');
     if(isMobil()) {
-        //bodyId.style.background = 'blue';
         elementSpanToReplace.style.top = '25%';
         elementSpanToReplace.style.left = '25%'
     }
     else {
-       // bodyId.style.background = 'red'
         elementSpanToReplace.style.top = '38%';
-        elementSpanToReplace.style.left = '40%';
+        elementSpanToReplace.style.left = '52%';
     }   
     elementSpanToReplace.style.transform = 'scale(1.85)';
+    elementSpanToReplace.style.display = 'block';
  
     // Concat is not clean, but works!
     elementSpanToReplace.src = elementSpanToReplace.src + '&TIME_SPAN=' + timeSpan;
