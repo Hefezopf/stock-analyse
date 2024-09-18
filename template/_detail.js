a814bdeef51d76f0f342457366901e430da99ffa
Markus Hopf <info@hopf-it.de>
2024-09-18 13:43:03 +0200

    Update _detail.js

--- template/_detail.js
+++ template/_detail.js
@@ -8,7 +8,7 @@ function showChart(timeSpan) {
 
     if(isMobil()) {
         elementSpanToReplace.style.top = '25%';
-        elementSpanToReplace.style.left = '25%';
+        elementSpanToReplace.style.left = '23%'
     }
     else {
         elementSpanToReplace.style.top = '38%';
