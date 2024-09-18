75be23926640016cfd24a7a637865a29aac71f6d
Markus Hopf <info@hopf-it.de>
2024-09-18 10:48:52 +0200

    style='display:none;position:fixed

--- template/_detail.js
+++ template/_detail.js
@@ -1,9 +1,12 @@
 // 
 // _detail.js
 // 
+
 function showChart(timeSpan) {
     var elementSpanToReplace = document.getElementById('imgToReplace');
     elementSpanToReplace.style.display = 'block';
+    // ?Hier muss unterschieden werden, ob Mobil oder PC-Browser?
+    elementSpanToReplace.style.top = '40%';
     elementSpanToReplace.style.left = '26%';
     elementSpanToReplace.style.transform = 'scale(1.85)';
  
