23a2eb7469f3e876e3be5e16233671008e6a4d40
Markus Hopf <info@hopf-it.de>
2024-09-26 12:09:26 +0200

    mte

--- script/start-all-in-chrome.sh
+++ script/start-all-in-chrome.sh
@@ -752,6 +752,9 @@ start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-an
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/EUNA.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/SEC0.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/C005.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/MTE.html
+
+
 
 
 read -p "Close Chrome manually and Press enter to continue the next 50"
