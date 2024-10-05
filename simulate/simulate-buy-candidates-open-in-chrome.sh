559187b8c01dc9f8550599b6212b5f5289bd62e5
Markus Hopf <info@hopf-it.de>
2024-10-05 16:39:47 +0200

    wf

--- simulate/simulate-buy-candidates-open-in-chrome.sh
+++ simulate/simulate-buy-candidates-open-in-chrome.sh
@@ -1,2 +1,1 @@
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/GE9.html
-start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/BM8.html
