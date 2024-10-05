e2e2cad86c1230de2215184dc09edd7c655f0625
Markus Hopf <info@hopf-it.de>
2024-10-05 17:23:59 +0200

    echo ""

--- simulate/simulate-buy-candidates-open-in-chrome.sh
+++ simulate/simulate-buy-candidates-open-in-chrome.sh
@@ -1,2 +1,3 @@
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/AXNA.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/GE9.html
-start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/BM8.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/0QF.html
--- simulate/simulate-buy-candidates.sh
+++ simulate/simulate-buy-candidates.sh
@@ -31,6 +31,23 @@ countSymbols=$((countSymbols + 1))
 echo ""
 echo "Symbols($countSymbols)"
 
+rm -rf "$SIM_LAST_ALARMS_HTML_FILE"
+HTML_FILE_HEADER="<!DOCTYPE html><html lang='en'>
+<head>
+<meta charset='utf-8' />
+<meta http-equiv='cache-control' content='no-cache, no-store, must-revalidate' />
+<meta http-equiv='pragma' content='no-cache' />
+<meta http-equiv='expires' content='0' />
+<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
+<link rel='stylesheet' href='_result.css' />
+<title>Simulate Last Alarms</title>
+</head>
+<body>
+<div>"
+echo "$HTML_FILE_HEADER" >> "$SIM_LAST_ALARMS_HTML_FILE"
+echo "Simulate Last Alarms" >> "$SIM_LAST_ALARMS_HTML_FILE"
+echo "<br><br>" >> "$SIM_LAST_ALARMS_HTML_FILE"
+
 # Simulate stocks for each symbol
 for symbol in $symbolsParam
 do
@@ -50,40 +67,19 @@ do
             symbolName=$(echo "$lineFromTickerFile" | cut -f 2)
             echo "Last '$lastDaysParam' Alarms for $symbol $symbolName: $lastAlarms" # Sample -> last 3 Alarms: 'C+5R+6S+M+','C+5R+6S+M+','C+5R+6S+M+'
             echo "start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/$symbol.html" >> ./simulate/simulate-buy-candidates-open-in-chrome.sh
-
             # TODO: if more then 50 -> build in!
             # echo "read -p 'Close Chrome manually and Press enter to continue the next 50'" >> ./simulate/simulate-buy-candidates-open-in-chrome.sh
+
+            echo "<a href='https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/$symbol.html'>$symbol</a><br>" >> "$SIM_LAST_ALARMS_HTML_FILE"
         fi
     fi
 done
-
-rm -rf "$SIM_LAST_ALARMS_HTML_FILE"
-HTML_FILE_HEADER="<!DOCTYPE html><html lang='en'>
-<head>
-<meta charset='utf-8' />
-<meta http-equiv='cache-control' content='no-cache, no-store, must-revalidate' />
-<meta http-equiv='pragma' content='no-cache' />
-<meta http-equiv='expires' content='0' />
-<link rel='shortcut icon' type='image/ico' href='favicon.ico' />
-<link rel='stylesheet' href='_result.css' />
-<title>Simulate Last Alarms</title>
-</head>
-<body>
-<div>"
-echo "$HTML_FILE_HEADER" >> "$SIM_LAST_ALARMS_HTML_FILE"
-echo "Simulate Last Alarms" >> "$SIM_LAST_ALARMS_HTML_FILE"
-echo "<br><br>" >> "$SIM_LAST_ALARMS_HTML_FILE"
-
-cat "simulate/simulate-buy-candidates-open-in-chrome.sh" >> "$SIM_LAST_ALARMS_HTML_FILE"
-
 echo "<br></div></body></html>" >> "$SIM_LAST_ALARMS_HTML_FILE"
-
-sed -i "s/start chrome /<a href='/g" "$SIM_LAST_ALARMS_HTML_FILE"
-sed -i "s/\.html/\.html'\>XXX<\/a\><br\>/g" "$SIM_LAST_ALARMS_HTML_FILE"
-
+echo ""
 if [ ! "$(uname)" = 'Linux' ]; then
     echo "./simulate/simulate-buy-candidates-open-in-chrome.sh" | clip
     echo "Call: ./simulate/simulate-buy-candidates-open-in-chrome.sh (in Clipboard)"
 fi
 echo "file:///D:/code/stock-analyse/simulate/out/_simulate_last_alarms.html"
 echo "https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/_simulate_last_alarms.html"
+
