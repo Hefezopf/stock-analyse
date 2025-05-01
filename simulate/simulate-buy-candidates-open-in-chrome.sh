73cd7dd838587ebaeefa5154b3085d54ed1e0c8b
Markus Hopf <info@hopf-it.de>
2025-05-01 05:58:37 +0000

    Github schedule

--- simulate/simulate-buy-candidates-open-in-chrome.sh
+++ simulate/simulate-buy-candidates-open-in-chrome.sh
@@ -1,2 +1,1 @@
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/UNH.html
-start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/PEP.html
