6ba6edd48102ab49eb8c578e1a99ac80d86119ec
Markus Hopf <info@hopf-it.de>
2025-09-11 09:32:20 +0200

    DBXT	"Xtr Tagesgeld Aussch

--- script/start-all-in-chrome.sh
+++ script/start-all-in-chrome.sh
@@ -940,3 +940,4 @@ start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-an
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/N0XA.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/669.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/F5D.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/DBXT.html
