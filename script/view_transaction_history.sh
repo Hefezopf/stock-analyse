e6cac5de0c69ce36a815c5ff0ae6fe8e5b73b953
Markus Hopf <info@hopf-it.de>
2025-09-19 13:11:26 +0200

    vt

--- script/view_transaction_history.sh
+++ script/view_transaction_history.sh
@@ -2,10 +2,11 @@
 
 # View transaction history from file config/transaction_history.txt
 #
-# Call: . script/view_transaction_history.sh SYMBOL | DATE
+# Call: . script/view_transaction_history.sh SYMBOL | DATE | Searchstring
 # Example: . script/view_transaction_history.sh BEI
 # Example: . script/view_transaction_history.sh 2022-03-
 # Example: . script/view_transaction_history.sh 2022
+# Example: . script/view_transaction_history.sh Lululemon
 # alias vt='/d/code/stock-analyse/script/view_transaction_history.sh $1'
 
 #set -x
@@ -15,16 +16,24 @@
 . script/constants.sh
 
 # To uppercase
-symbolParam=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+#symbolParam=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+symbolParam=$1
 
 echo "View Transaction History $symbolParam"
+echo ""
 
 lineFromFile=$(grep -F "$symbolParam" "$TRANSACTION_HISTORY_FILE")
-# BEI	2022-04-23	352	SELL	"BEIERSDORF"
-priceFromFile=$(echo "$lineFromFile" | cut -f 3)
-#echo "$priceFromFile€"
+# &nbsp;2025-09-19	<span>245&euro;</span>	4.8%	<a href='https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/RCF.html' target='_blank'>RCF	"Teleperformance"</a><br>
+
 echo "$lineFromFile"
 
+#<span>245&euro;</span>
+priceFromFile=$(echo "$lineFromFile" | cut -f 2)
+
+priceFromFile=$(echo "$priceFromFile" | sed 's/%//g')
+priceFromFile=$(echo "$priceFromFile" | sed 's/<span>//g')
+  
 summe=$(echo "$priceFromFile" | awk '{s += $1;} END {print s;}')
 
+echo ""
 echo "Sum before tax: $summe€"
