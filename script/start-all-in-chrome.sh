af41d4c93746b14657ed12b5b62ffc5ad3ae0755
Markus Hopf <info@hopf-it.de>
2025-10-21 08:09:48 +0200

    529619884

--- script/start-all-in-chrome.sh
+++ script/start-all-in-chrome.sh
@@ -954,3 +954,4 @@ start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-an
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/0R1.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/AIFS.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/OBCK.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/TKMS.html
