198c57c2ae658d76ccab236fa734d72c109c531d
Markus Hopf <info@hopf-it.de>
2025-10-23 20:43:33 +0200

    lineFromFile=$(echo "$lineFromFile" | sed 's/<//g')

--- script/view_transaction_history.sh
+++ script/view_transaction_history.sh
@@ -24,7 +24,6 @@ echo ""
 
 lineFromFile=$(grep -F "$symbolParam" "$TRANSACTION_HISTORY_FILE")
 #<div style='font-size: x-large;'>&nbsp;2025-10-03&#9;133&#8364;&#9;4.9%&#9;<a href='https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/58H.html' target='_blank'>58H&#9;"Davide-Campari"</a></div><br>
-echo "$lineFromFile"
 
 # shellcheck disable=SC2001
 lineFromFile=$(echo "$lineFromFile" | sed 's/&#9;/\t/g')
@@ -39,5 +38,26 @@ priceFromFile=$(echo "$priceFromFile" | sed 's/&#9;/\t/g')
   
 summe=$(echo "$priceFromFile" | awk '{s += $1;} END {print s;}')
 
+lineFromFile=$(echo "$lineFromFile" | sed 's/<div>//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/&nbsp;/ /g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/<\/a><\/div>//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\/\/htmlpreview//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/target=//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/_blank//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/https//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/github//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/Hefezopf\/stock-analyse//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\/blob\/main\/out//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\t/ /g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/a href=//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/&#8364;/€/g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/<//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/>//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\.html//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\.com//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/\.io//g')
+lineFromFile=$(echo "$lineFromFile" | sed 's/:.\/?:\/\/\/\///g')
+
+echo "$lineFromFile"
 echo ""
 echo "Sum before tax: $summe€"
