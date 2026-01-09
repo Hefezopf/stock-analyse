deae34513f03674efa7603c81516192bc3aea8db
Markus Hopf <markus@hopf-it.de>
2026-01-09 10:11:37 +0100

    Legal&General

--- script/start-all-in-chrome.sh
+++ script/start-all-in-chrome.sh
@@ -1007,3 +1007,4 @@ start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-an
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/EXH3.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/FVJ.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/HLD.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/LGI.html
