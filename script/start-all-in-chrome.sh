b78b765ba509091fb97b052457906f4ee013acef
Markus Hopf <markus@hopf-it.de>
2026-02-28 09:05:33 +0100

    7RM

--- script/start-all-in-chrome.sh
+++ script/start-all-in-chrome.sh
@@ -1031,3 +1031,4 @@ start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-an
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/BZY.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/ABS2.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/EVN.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/7RM.html
