271dc9513b7e39b51f26447c3e0a591a72640bbf
Markus Hopf <info@hopf-it.de>
2026-05-29 05:26:00 +0000

    Github schedule

--- simulate/simulates-last-x-days-y-alarms-open-in-chrome.sh
+++ simulate/simulates-last-x-days-y-alarms-open-in-chrome.sh
@@ -1,2 +1,7 @@
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/GRU.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/L10.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/AZ5.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/FRE.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/NNND.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/NWL.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/out/RGO.html
