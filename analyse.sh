cb2f299ba988f9fd40a31dc9de4b1d639e7f573b
Markus Hopf <markus@hopf-it.de>
2026-07-07 19:04:43 +0200

    &nbsp;

--- analyse.sh
+++ analyse.sh
@@ -612,10 +612,10 @@ do
         echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('10D')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_10D""$ID_NOTATION"\" " target=\"_blank\">$markerOwnStock$symbol $symbolName</a>"
         echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('6M')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_6M""$ID_NOTATION"\" " target=\"_blank\">&nbsp;6M&nbsp;</a>"
 #        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
-        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y</a>"
+        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
 
 #        echo "&nbsp;&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:rgb(0, 0, 0)'><b>$last€</b></span>"
-        echo "&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:rgb(0, 0, 0)'><b>$last€</b></span>"
+        echo "<span style=';font-size:$FONT_SIZE_DETAIL;color:rgb(0, 0, 0)'><b>$last€</b></span>"
 
         percentLastDay=$(echo "$last $beforeLastQuote" | awk '{print ((($1 / $2)-1)*100)}')
         percentLastDay=$(printf "%.1f" "$percentLastDay")
@@ -625,7 +625,7 @@ do
             _linkColor="$RED"
         fi
 
-        echo "&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:$_linkColor'><b>""$percentLastDay""%</b></span>"
+        echo "<span style=';font-size:$FONT_SIZE_DETAIL;color:$_linkColor'><b>""$percentLastDay""%</b></span>"
         echo "</p>"
 #############
 
@@ -717,9 +717,9 @@ do
 #############
         # echo "<a id='headlineLink$symbol' $styleComdirectLink onmouseover=\"javascript:showChart('10D')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_10D""$ID_NOTATION"\" " target=\"_blank\">$markerOwnStock$symbol $symbolName</a>"
         # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('6M')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_6M""$ID_NOTATION"\" " target=\"_blank\">&nbsp;6M&nbsp;</a>"
-        # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y</a>"
-        # echo "&nbsp;<span style='font-size:$FONT_SIZE_DETAIL; color:rgb(0, 0, 0)'><b>$last€</b></span>"
-        # echo "&nbsp;<span style='font-size:$FONT_SIZE_DETAIL; color:$_linkColor'><b>""$percentLastDay""%</b></span><br>" 
+        # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
+        # echo "<span style='font-size:$FONT_SIZE_DETAIL; color:rgb(0, 0, 0)'><b>$last€</b></span>"
+        # echo "<span style='font-size:$FONT_SIZE_DETAIL; color:$_linkColor'><b>""$percentLastDay""%</b></span><br>" 
 #############
 
         if [ "$asset_type" = 'STOCK' ] || [ "$asset_type" = 'INDEX' ]; then
@@ -743,7 +743,8 @@ do
             .tooltip {
                 display: inline-block;
                 border-bottom: 2px dotted black; 
-                font-size: $FONT_SIZE_DETAIL;
+                // font-size: $FONT_SIZE_DETAIL;
+                font-size: 50px;
                 line-height: 37px;
                 vertical-align: baseline;
             }
