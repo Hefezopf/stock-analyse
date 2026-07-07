b72f565874cfd566b030f022a14c4b8d93ba9bc9
Markus Hopf <markus@hopf-it.de>
2026-07-07 17:13:01 +0200

    style="width:107%; margin-left: -55px;

--- analyse.sh
+++ analyse.sh
@@ -604,26 +604,25 @@ do
             COMDIRECT_URL_5Y="$COMDIRECT_URL_INDEX_PREFIX_5Y"
         fi
 
-        echo "<p style='text-align:right; font-size:$FONT_SIZE_DETAIL; margin:1px 1px 1px 1px'>"
-#        echo "<p style='text-align:right; font-size:$FONT_SIZE_DETAIL; padding: 1px 1px 1px 1px;'>"
-        # onmouseenter vs. onmouseover
-        # onmouseleave vs. onmouseout
-        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('10D')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_10D""$ID_NOTATION"\" " target=\"_blank\">$markerOwnStock$symbol $symbolName</a>"
-        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('6M')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_6M""$ID_NOTATION"\" " target=\"_blank\">&nbsp;6M&nbsp;</a>"
-        echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
+        # echo "<p style='text-align:right; font-size:$FONT_SIZE_DETAIL; margin:1px 1px 1px 1px'>"
+        # # onmouseenter vs. onmouseover
+        # # onmouseleave vs. onmouseout
+        # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('10D')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_10D""$ID_NOTATION"\" " target=\"_blank\">$markerOwnStock$symbol $symbolName</a>"
+        # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('6M')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_6M""$ID_NOTATION"\" " target=\"_blank\">&nbsp;6M&nbsp;</a>"
+        # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
 
-        echo "&nbsp;&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:rgb(0, 0, 0)'><b>$last€</b></span>"
+        # echo "&nbsp;&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:rgb(0, 0, 0)'><b>$last€</b></span>"
 
-        percentLastDay=$(echo "$last $beforeLastQuote" | awk '{print ((($1 / $2)-1)*100)}')
-        percentLastDay=$(printf "%.1f" "$percentLastDay")
-        isPercentLastDayNegativ=${percentLastDay:0:1}
-        _linkColor="$GREEN"
-        if [ "$isPercentLastDayNegativ" = '-' ]; then
-            _linkColor="$RED"
-        fi
+        # percentLastDay=$(echo "$last $beforeLastQuote" | awk '{print ((($1 / $2)-1)*100)}')
+        # percentLastDay=$(printf "%.1f" "$percentLastDay")
+        # isPercentLastDayNegativ=${percentLastDay:0:1}
+        # _linkColor="$GREEN"
+        # if [ "$isPercentLastDayNegativ" = '-' ]; then
+        #     _linkColor="$RED"
+        # fi
 
-        echo "&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:$_linkColor'><b>""$percentLastDay""%</b></span>"
-        echo "</p>"
+        # echo "&nbsp;<span style=';font-size:$FONT_SIZE_DETAIL;color:$_linkColor'><b>""$percentLastDay""%</b></span>"
+        # echo "</p>"
         
         cat template/indexPart1a.html
 
