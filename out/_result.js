12e155f1015ee1ffa5f2147a6bbb49a909c06a8b
Markus Hopf <markus@hopf-it.de>
2026-07-07 13:07:01 +0200

    scale(1.4)'; // yyy

--- out/_result.js
+++ out/_result.js
@@ -838,7 +838,7 @@ function showChart(timeSpan, symbol) {
     elementSpanToReplace.style.display = 'block';
     elementSpanToReplace.style.top = '19%';
     elementSpanToReplace.style.left = '17%';
-    elementSpanToReplace.style.transform = 'scale(1.5)';  
+    elementSpanToReplace.style.transform = 'scale(1.4)'; // yyy
     // Concat is not clean, but works!
     elementSpanToReplace.src = elementSpanToReplace.src + '&TIME_SPAN=' + timeSpan;
 }
