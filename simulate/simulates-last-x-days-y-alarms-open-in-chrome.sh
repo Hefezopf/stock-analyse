36ec2dfc7863aed08db8c92a3da4af9a93876c42
Markus Hopf <markus@hopf-it.de>
2026-07-07 08:41:41 +0200

    font-size: 310%;

--- simulate/simulates-last-x-days-y-alarms-open-in-chrome.sh
+++ simulate/simulates-last-x-days-y-alarms-open-in-chrome.sh
@@ -1,1 +1,6 @@
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/2OY.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/8CW.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/G14.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/EIA.html
+start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/NOH1.html
 start chrome https://htmlpreview.github.io/?https://github.com/Hefezopf/stock-analyse/blob/main/simulate/out/7DB.html
