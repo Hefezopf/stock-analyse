1bfbd20ed71166b947396442ccae116b895f0ace
Markus Hopf <markus@hopf-it.de>
2026-07-08 10:37:37 +0200

    percentLastDay

--- analyse.sh
+++ analyse.sh
@@ -604,6 +604,13 @@ do
             COMDIRECT_URL_5Y="$COMDIRECT_URL_INDEX_PREFIX_5Y"
         fi
 
+        percentLastDay=$(echo "$last $beforeLastQuote" | awk '{print ((($1 / $2)-1)*100)}')
+        percentLastDay=$(printf "%.1f" "$percentLastDay")
+        isPercentLastDayNegativ=${percentLastDay:0:1}
+        _linkColor="$GREEN"
+        if [ "$isPercentLastDayNegativ" = '-' ]; then
+            _linkColor="$RED"
+        fi
 
 #############
         # echo "<p style='text-align:right;font-size:$FONT_SIZE_DETAIL;margin:-1px 0px 0px 0px'>"
@@ -612,21 +619,10 @@ do
         # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('10D')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_10D""$ID_NOTATION"\" " target=\"_blank\">$markerOwnStock$symbol $symbolName</a>"
         # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('6M')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_6M""$ID_NOTATION"\" " target=\"_blank\">&nbsp;6M&nbsp;</a>"
         # echo "<a $styleComdirectLink onmouseover=\"javascript:showChart('5Y')\" onmouseout='javascript:hideChart()' href=\"$COMDIRECT_URL_5Y""$ID_NOTATION"\" " target=\"_blank\">&nbsp;5Y&nbsp;</a>"
-
         # echo "<span style=';font-size:40px;color:rgb(0, 0, 0)'><b>$last€</b></span>"
-
-        # percentLastDay=$(echo "$last $beforeLastQuote" | awk '{print ((($1 / $2)-1)*100)}')
-        # percentLastDay=$(printf "%.1f" "$percentLastDay")
-        # isPercentLastDayNegativ=${percentLastDay:0:1}
-        # _linkColor="$GREEN"
-        # if [ "$isPercentLastDayNegativ" = '-' ]; then
-        #     _linkColor="$RED"
-        # fi
-
         # echo "<span style=';font-size:40px;color:$_linkColor'><b>""$percentLastDay""%</b></span>"
         # echo "</p>"
 #############
-
 
         cat template/indexPart1a.html
 
