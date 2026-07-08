c3b761417e1612da13e39fb6a2bab42cfde13672
Markus Hopf <info@hopf-it.de>
2026-07-08 11:56:49 +0000

    Github push

--- out/_result.js
+++ out/_result.js
@@ -783,13 +783,13 @@ function showChart(timeSpan, symbol) {
     elementSpanToReplace.style.display = 'block';
 
     if(isMobil()) {
-        elementSpanToReplace.style.top = '19%';   
+        elementSpanToReplace.style.top = '15%';
         elementSpanToReplace.style.left = '11%';
         elementSpanToReplace.style.transform = 'scale(1.25)'; // yyy      
     }
     else {
-        elementSpanToReplace.style.top = '12%';
-        elementSpanToReplace.style.left = '15%';    
+        elementSpanToReplace.style.top = '10%';
+        elementSpanToReplace.style.left = '15%';
         elementSpanToReplace.style.transform = 'scale(1.1)'; // yyy
     }
 
