6239a885b2275333cc5e9a7568a301f743c8fef1
Markus Hopf <markus@hopf-it.de>
2026-07-09 08:33:36 +0200

    elementSpanToReplace.style.top = '15%';

--- template/_detail.js
+++ template/_detail.js
@@ -5,14 +5,14 @@
 function showChart(timeSpan) {
     var elementSpanToReplace = document.getElementById('imgToReplace');
     if(isMobil()) {
-        elementSpanToReplace.style.top = '25%';
+        elementSpanToReplace.style.top = '15%';
         elementSpanToReplace.style.left = '20%'
         elementSpanToReplace.style.transform = 'scale(1.7)'; // yyy
        // elementSpanToReplace.style.transform = 'scale(1.85)'; // yyy        
     }
     else {
         elementSpanToReplace.style.top = '9%';
-        elementSpanToReplace.style.left = '34%';
+        elementSpanToReplace.style.left = '44%';
         elementSpanToReplace.style.transform = 'scale(1.2)';
     }   
     elementSpanToReplace.style.display = 'block';
