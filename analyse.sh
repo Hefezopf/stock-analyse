0de151d595c2226dd52ea037be80218a6e2ce3ec
Markus Hopf <markus@hopf-it.de>
2026-07-10 21:43:28 +0200

    # cccc

--- analyse.sh
+++ analyse.sh
@@ -763,7 +763,16 @@ do
             else
                 # echo "<span><img id='countryflagId' alt='${isin:0:2}: No Flag' title='${isin:0:2}' src='../image/flags/${isin:0:2}.jpeg' width='5%' height='5%' style='border:1px solid;'></span>&nbsp;&nbsp;&nbsp;"
                 #echo "<span class='tooltiptext'><img id='countryflagId' alt='${isin:0:2}: No Flag' title='${firmenportrait}' src='../image/flags/${isin:0:2}.jpeg' width='5%' height='5%' style='border:1px solid;'></span>&nbsp;&nbsp;&nbsp;"
-                echo "<span style='font-size:$FONT_SIZE_DETAIL'><img id='countryflagId' alt='${isin:0:2}: No Flag' title='${isin:0:2}:${firmenportrait}' src='../image/flags/${isin:0:2}.jpeg' width='5%' height='5%' style='border:1px solid;'>&nbsp;$branche</span>"
+  #              echo "<span style='font-size:$FONT_SIZE_DETAIL'><img id='countryflagId' alt='${isin:0:2}: No Flag' title='${isin:0:2}:${firmenportrait}' src='../image/flags/${isin:0:2}.jpeg' width='5%' height='5%' style='border:1px solid;'>&nbsp;$branche</span>"
+               # echo "<div class='tooltip'><div class='tooltiptext'><img class='tooltiptext' id='countryflagId' alt='${isin:0:2}: No Flag' title='${isin:0:2}:${firmenportrait}' src='../image/flags/${isin:0:2}.jpeg' width='5%' height='5%' style='border:1px solid;'>&nbsp;$branche</div></div>"
+
+
+# cccc
+echo "<span class='tooltip' style='font-size:$FONT_SIZE_DETAIL' onclick='this.classList.toggle('active')'>
+        <img src='../image/flags/${isin:0:2}.jpeg' alt='${isin:0:2}: No Flag' width='5%' height='5%' style='border:1px solid;'>
+        <span class='tooltiptext'>${isin:0:2}:${firmenportrait}</span>
+      &nbsp;$branche</span>"   
+
             fi
 
             #echo "<div>$branche<br></div></p>"
